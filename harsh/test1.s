6699c498af23841615e8a411aa3b434c6bd5f69a
GoodDeeds <raosukrut@gmail.com>
2016-10-05 20:28:25 +0530

    Added sample tests and sent outputs to original samples to files

--- harsh/test1.s
+++ harsh/test1.s
@@ -1,19 +1,35 @@
 .globl main
-.data 
-m: .word -10 , -5 , -9 , -11 , 456 , 986 , -96 , -456 , 78, 45  #array 
-size  : .word 9#array size
+.data
+m1: .word -10
+m2: .word -5
+m3: .word -9
+m4: .word -11
+m5: .word 456  #array
+size  : .word 5#array size
 .text
 main:
-	la $t0,m #to store the array address
+	addi $sp, $sp, -16
+	lw $t0, m1
+	sw $t0, 0($sp)
+	lw $t0, m2
+	sw $t0, 4($sp)
+	lw $t0, m3
+	sw $t0, 8($sp)
+	lw $t0, m4
+	sw $t0, 12($sp)
+	lw $t0, m5
+	sw $t0, 16($sp)
+	addi $t8, $zero, 4
+	add $t0, $sp, $zero#to store the array address
 	lw $s1,size#to store the size in s1
 	lw $s3,0($t0)#this is my maximum variable , assuming that 1st one is max
-	li $t2, 0#the loop variable
-	li $t7, 1#to store the array offset to get value
+	addi $t2, $zero, 0#the loop variable
+	addi $t7, $zero, 1#to store the array offset to get value
 	addi $s1, $s1, -1
 	loop:#loop label
 		slt $t3, $t2, $s1#comparing if loop should be executed or not
 		beq $t3, $zero, exit#checking
-		sll $t4, $t7, 2#left shift for array offset
+		mul $t4, $t7, $t8#left shift for array offset
 		add $t0, $t0, $t4#updating loop variable
 		lw $t5, 0($t0)#storing loop value
 		slt $t6, $t5, $s3#comparing for max
@@ -22,5 +38,5 @@ main:
 	exitIF:
 		addi $t2,$t2, 1#update loop variaable
 		j loop		#loop jump
-	exit:  
+	exit:
 halt
